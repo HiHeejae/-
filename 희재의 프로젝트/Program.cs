5fa24ab0a8f8c326df42698f79be6ec355647cb4
HiHeejae <aag391345@gmail.com>
2024-03-12 11:17:51 +0900

    giftbox 추가
    Letter와 Money를 넣고 배송기사님도 넣어줬다

--- 희재의 프로젝트/Program.cs
+++ 희재의 프로젝트/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace 희재의_프로젝트
+﻿using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+
+namespace 희재의_프로젝트
 {
+    class Giftbox
+    {
+        public string Letter;
+        public int Money;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Giftbox a = new Giftbox()
+            {
+                Letter = "잘 지내지?",
+                Money = 100000
+            };
+            Giftbox 배송기사님 = a;
+
+            a.Letter = "어떻게 지내고 있어?";
+            a.Money = 150000;
         }
     }
 }
